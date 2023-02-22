1c5207cf68c53bc453492daa3d6f19dad5252b2a
IyanuGithub <owolabiiyanu94@gmail.com>
2023-02-22 12:35:23 +0100

    second commit

--- controller/teamController.js
+++ controller/teamController.js
@@ -1,7 +1,7 @@
 const Teams = require('../models/team')
 
 const getAllTeams = async (req, res) => {
-    const {name ,location, uclwinner, sort, select} = req.query
+    const {name ,location, uclwinner, sort, select, numberFilters} = req.query
     console.log(req.query)
     let queryObject = {}
     let result = Teams.find(queryObject)
@@ -13,6 +13,28 @@ const getAllTeams = async (req, res) => {
         }
         if (uclwinner){
             queryObject.uclwinner = uclwinner === "true" ? true : false
+        }
+        if (numberFilters) {
+            const operatorMap = {
+                '>': '$gt',
+                '>=': '$gte',
+                '=': '$eq',
+                '<': '$lt',
+                '<=': '$lte'
+            }
+            const regEx = /\b(<|>|>=|<=|=)\b/g;
+            //ratings>3.0
+            let filters = numberFilters.replace(
+                regEx,
+                (match) => `-${operatorMap[match]}-`
+            )
+            const options = ['rating']
+            filters = filters.split(',').forEach((item) => {
+                const [search, operator, value] = item.split('-')
+                if(options.includes(search)) {
+                    queryObject[search] = { [operator]: Number(value) }
+                }
+            })
         }
 
         //sorting
@@ -28,7 +50,7 @@ const getAllTeams = async (req, res) => {
         //limit
           const limit = Number(req.query.limit)
           result = result.limit(limit)
-          
+
     result = result.find(queryObject)
     const teams = await result
     res.status(200).json({ noOfTeams: teams.length, teams})
